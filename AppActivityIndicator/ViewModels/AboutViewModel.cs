337074fb89c9337ce7f4e1f00f4e4de2620f22d1
thanhvo <thanhvo.dev@gmail.com>
2022-06-22 10:23:03 +0700

    HomePage

--- AppActivityIndicator/ViewModels/AboutViewModel.cs
+++ AppActivityIndicator/ViewModels/AboutViewModel.cs
@@ -9,8 +9,24 @@ namespace AppActivityIndicator.ViewModels
     {
         public AboutViewModel()
         {
-            Title = "About";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            Title = "HCMUT";
+            OpenWebCommand = new Command(async () =>
+            {
+                if (Device.RuntimePlatform == Device.iOS)
+                {
+                    // https://developer.apple.com/library/ios/featuredarticles/iPhoneURLScheme_Reference/MapLinks/MapLinks.html
+                    await Launcher.OpenAsync("http://maps.apple.com/?q=394+Pacific+Ave+San+Francisco+CA");
+                }
+                else if (Device.RuntimePlatform == Device.Android)
+                {
+                    // open the maps app directly
+                    await Launcher.OpenAsync("geo:0,0?q=HCMC+National+University+Dormitary+Zone+A");
+                }
+                else if (Device.RuntimePlatform == Device.UWP)
+                {
+                    await Launcher.OpenAsync("bingmaps:?where=394 Pacific Ave San Francisco CA");
+                }
+            });
             CallSupportCommand = new Command(() => PhoneDialer.Open("1945724833"));
         }
 
