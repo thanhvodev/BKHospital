6c7f2ea4e19c4519dc634072efcfbc2680b8742f
thanhvo <thanhvo.dev@gmail.com>
2022-06-23 14:40:42 +0700

    reset password

--- AppActivityIndicator/Views/LoginPage.xaml.cs
+++ AppActivityIndicator/Views/LoginPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using AppActivityIndicator.ViewModels;
+using Firebase.Auth;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +22,21 @@ namespace AppActivityIndicator.Views
         private async void TapGestureRecognizer_Tapped_SignUp(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new SignUpPage());
+        }
+
+        private async void TapGestureRecognizer_Tapped_Reset(object sender, EventArgs e)
+        {
+            string result = await DisplayPromptAsync("Email", "Nhập Email của bạn để đổi mật khẩu");
+            FirebaseAuthProvider authProvider = new FirebaseAuthProvider(new FirebaseConfig(Constants.WebAPIkey));
+            try
+            {
+                await authProvider.SendPasswordResetEmailAsync(result);
+                await DisplayAlert("Thành công", "Vào hộp thư của bạn để đổi mật khẩu", "OK");
+            }
+            catch (Exception)
+            {
+                await DisplayAlert("Có lỗi xảy ra", "Kiểm tra lại Email của bạn", "OK");
+            }
         }
     }
 }
