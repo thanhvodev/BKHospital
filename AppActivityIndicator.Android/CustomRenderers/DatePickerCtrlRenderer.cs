b81d6cd99bb966df7e5c4dbe7d3fe9701143f475
thanhvo <thanhvo.dev@gmail.com>
2022-07-01 11:43:50 +0700

    UI for making appointment page

---
+++ AppActivityIndicator.Android/CustomRenderers/DatePickerCtrlRenderer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using AppActivityIndicator.CustomControls;
+using DatePickerDefaultTextDemo.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+#pragma warning disable CS0612 // Type or member is obsolete
+[assembly: ExportRenderer(typeof(DatePickerCtrl), typeof(DatePickerCtrlRenderer))]
+#pragma warning restore CS0612 // Type or member is obsolete
+namespace DatePickerDefaultTextDemo.Droid
+{
+    [Obsolete]
+    public class DatePickerCtrlRenderer : DatePickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+        {
+            base.OnElementChanged(e);
+            this.Control.SetTextColor(Android.Graphics.Color.Rgb(83, 63, 149));
+            this.Control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+            this.Control.SetPadding(20, 0, 0, 0);
+
+            GradientDrawable gd = new GradientDrawable();
+            gd.SetCornerRadius(25); //increase or decrease to changes the corner look  
+            gd.SetColor(Android.Graphics.Color.Transparent);
+            gd.SetStroke(3, Android.Graphics.Color.Rgb(83, 63, 149));
+
+            this.Control.SetBackgroundDrawable(gd);
+
+            DatePickerCtrl element = Element as DatePickerCtrl;
+
+            if (!string.IsNullOrWhiteSpace(element.Placeholder))
+            {
+                Control.Text = element.Placeholder;
+            }
+
+            this.Control.TextChanged += (sender, arg) => {
+                var selectedDate = arg.Text.ToString();
+                if (selectedDate == element.Placeholder)
+                {
+                    Control.Text = DateTime.Now.ToString("dd/MM/yyyy");
+                }
+            };
+        }
+    }
+}
