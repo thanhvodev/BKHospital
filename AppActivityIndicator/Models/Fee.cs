4c9774187b81a8d3866b40d5e270d4ea760cb493
thanhvo <thanhvo.dev@gmail.com>
2022-07-26 10:35:02 +0700

    fix medical sheet detail state bug

---
+++ AppActivityIndicator/Models/Fee.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppActivityIndicator.Models
+{
+    public class Fee
+    {
+        [PrimaryKey]
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public ulong PhiKhamBenh { get; set; }
+        public ulong PhiPhauThuat { get; set; }
+        public ulong PhiThuoc { get; set; }
+        public ulong PhiAnUong { get; set; }
+        public ulong PhiDieuDuong { get; set; }
+        public ulong PhiONoiTru { get; set; }
+
+    }
+}
